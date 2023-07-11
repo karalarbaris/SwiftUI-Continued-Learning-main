54c09b9378fd77511760df979975d8da7fabf81f
Baris Karalar <karalarbaris@gmail.com>
2023-07-11 09:17:42 +0200

    Initial Commit

---
+++ self/MultipleSheetsBootcampb.swift
@@ -0,0 +1,20 @@
+//
+//  MultipleSheetsBootcampb.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Baris Karalar on 11.07.23.
+//
+
+import SwiftUI
+
+struct MultipleSheetsBootcampb: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MultipleSheetsBootcampb_Previews: PreviewProvider {
+    static var previews: some View {
+        MultipleSheetsBootcampb()
+    }
+}
